64a4c9fc7e7fe5be3f87f42a55c89606afa95c87
atmak <atmakurunithesh911@gmail.com>
2024-08-19 21:36:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+DOCTYPE html>
+<html>
+<head>
+  <title>Your Title Here</title>
+</head>
+<body>
+  <h1>Hello World</h1>
+
+  <script>
+    alert("Hello, this is a simple JavaScript alert!");
+  </script>
+</body>
+</html>
